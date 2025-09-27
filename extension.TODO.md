79ac64a6118e04fff42247c34f6dd93bf8c5b548
Egbaiyelo <moteniolaegbaiyelo@trentu.ca>
2025-09-27 10:07:01 -0400

    Squashed commit of the following:
    commit 980c4a7b3f06920a15a2764e49e0350925f117c7
    Author: Egbaiyelo <123100338+Egbaiyelo@users.noreply.github.com>
    Date:   Sat Sep 27 09:56:54 2025 -0400
    
        Improved extension features (#2)
    
        * Update: Site logging
    
        Logging all the company names and sites that the extension comes across
    
        * Update: Application form handling sign in
    
        Handling application sign in and create account
    
        * Update: Connecting to nativeHost
    
        establishing connection to app
        Adding a test-app (slimmer for testing)
    
        * Chore: Reviewing and commenting the site logger
    
        Ensuring the site logging functionality works well
    
        * Chore: Reviewing and commenting the account autofiller
    
        * Update: Adding HomeLink Icon
    
        * Update: Adding target button to the page
    
        Testing how to add the target button to the page
    
        * Update: Adding Icon and text
    
        Adding the icon and the text and blending the styles in
    
        * Update: Button blend
    
        Ensuring the buttons have the right color and style and all
    
        * Adding myWorkday Home page
    
        Adding the button link to the home page
    
        * Update: Adding Account helper buttons
    
        Adding signIn and Register helper buttons
    
        * Update: Making the button last element in nav
    
        Ensuing my button is always last element in nav
    
        * Update background.js
    
        * Update: Restablishing connection with nativehost
    
        Fixing the connection with nativehost (still a persistent connection issue though).
        Tailoring the logic to what's needed.
    
        * Update: testing protocol update
    
        Also trying getData (scraping) and displaying in the home page
    
        * Update: Collecting and sending data with nativehost
    
        Managing received data from nativehost for scraping and sending data to nativehost for addsite
    
    commit ec16fa82b2302f07610f49ba79ef8e1178292df2
    Merge: 22bf269 8aca0b4
    Author: Egbaiyelo <moteniolaegbaiyelo@trentu.ca>
    Date:   Fri Sep 19 14:18:53 2025 -0400
    
        Merge branch 'main' of https://github.com/Egbaiyelo/Workday-Consolidate
    
    commit 8aca0b47fb92d7604145cd4667931e3b2443696a
    Author: Egbaiyelo <123100338+Egbaiyelo@users.noreply.github.com>
    Date:   Fri Sep 19 14:08:24 2025 -0400
    
        Basic extension features - Dev -0.1 (#1)
    
        * Setup and testing
    
        * Testing Add button
    
        Adding a button to the page to test structure and feasibility
    
        * Try for account Sign In functionality
    
        Adding ways to sign in
    
        * Update content.js
    
        * Adding Puppeteer
    
        * Adding Scraping logic
    
        * Change name and update README
    
        * Update: Scraping logic for looping and storage
    
        Adding more modularity for multiple pages and ensuring safe sign in to them all, if a site is already signed in, it just proceeds.
        More logic to ensure jobs exist and testing logic to scrape multiple sites and write to file, testing grabbing css, also storing the application data in json
    
        * Install: jsdom, express and cors
    
        Express for the server mockup and jsdom for html parsing
    
        * Update: Scraping logic and adding server
    
        * Update .gitignore
    
        * Chore: restructuring
    
        Separating into app and extension
    
        * Update: Site logging
    
        Logging all the company names and sites that the extension comes across
    
        * Update: Application form handling sign in
    
        Handling application sign in and create account
    
        * Update: Connecting to nativeHost
    
        establishing connection to app
        Adding a test-app (slimmer for testing)
    
        * Chore: Reviewing and commenting the site logger
    
        Ensuring the site logging functionality works well
    
        * Chore: Reviewing and commenting the account autofiller
    
        * Update: Adding HomeLink Icon

---
+++ extension.TODO.md
@@ -0,0 +1,17 @@
+
+Ensure logged sites are sites where user is logged in?
+Ensure the myworkday links to an actual page which reads the nativemessaging data
+<!-- Ensure signin help and log in help buttons -->
+Query and feed scraped data to HTML
+Design HTML pages (home and feedback and download page)
+Fix the add site function
+Manage errors
+- failed conenction
+- failed home.html load
+- failed fucntions, automatic reloads maybe (could be dangerour for user who could lose data)
+- etc
+
+
+## Future
+Allow users to save postings to apply to later?
+Log the last time visiting a site maybe
--- extension/background.js
+++ extension/background.js
@@ -1,85 +1,53 @@
+// globalThis.sendNativeCommand = sendNativeCommand;
+// globalThis.connectToNativeHost = connectToNativeHost;
+
+// globalThis.testNative = () => {
+//     sendNativeCommand('/scrape', { url: 'https://google.com' })
+//         .then(response => console.log('Scrape result:', response))
+//         .catch(error => console.error('Scrape error:', error));
+// };
 
 
-let nativePort = null;
+// maybe the action and message should be the same string
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === 'getCredentials') {
 
-chrome.runtime.onInstalled.addListener(() => {
-    console.log("$$...Installed");
-    connectToNativeHost();
-});
-
-function connectToNativeHost() {
-    console.log("Connecting to native host...");
-    
-    if (nativePort) {
-        nativePort.disconnect();
-    }
-    
-    nativePort = chrome.runtime.connectNative('com.me.my_workday');
-    
-    nativePort.onMessage.addListener((msg) => {
-        console.log("Received from native host:", msg);
-        
-        switch (msg.command) {
-            default:
-                console.warn("Received message: ", msg.result, "\nThis can't be handled");
-        }
-    });
-    
-    nativePort.onDisconnect.addListener(() => {
-        if (chrome.runtime.lastError) {
-            console.error("Native host disconnected with error:", chrome.runtime.lastError.message);
-            
-            setTimeout(() => {
-                console.log("Attempting to reconnect...");
-                connectToNativeHost();
-            }, 2000);
-        } else {
-            console.log("Native host disconnected normally");
-        }
-        nativePort = null;
-    });
-    
-    // maybe test connection
-}
-
-
-function sendNativeCommand(command, data) {
-    return new Promise((resolve, reject) => {
-        if (!nativePort) {
-            console.error("No native port connection for command:", command);
-            connectToNativeHost();
-            reject(new Error('No native connection'));
-            return;
-        }
-        
-        console.log(`Sending command: ${command}`, data);
-        
-        // Create a one-time listener for this specific command
-        const listener = (msg) => {
-            if (msg.command === `${command.replace('/', '')}_response`) {
-                nativePort.onMessage.removeListener(listener);
-                resolve(msg);
+        chrome.runtime.sendNativeMessage('com.me.my_workday', { action: '/get-credentials' }, (response) => {
+            if (chrome.runtime.lastError) {
+                console.error('Native messaging error:', chrome.runtime.lastError.message);
             }
-        };
-        
-        nativePort.onMessage.addListener(listener);
-        
-        setTimeout(() => {
-            nativePort.onMessage.removeListener(listener);
-            reject(new Error(`Command ${command} timed out`));
-        }, 5000);
-        
-        nativePort.postMessage({ command, data });
-    });
-}
 
-globalThis.sendNativeCommand = sendNativeCommand;
-globalThis.connectToNativeHost = connectToNativeHost;
+            console.log('Received from native app:', response);
+            sendResponse(response.result);
+        })
 
-globalThis.testNative = () => {
-    sendNativeCommand('/scrape', { url: 'https://google.com' })
-        .then(response => console.log('Scrape result:', response))
-        .catch(error => console.error('Scrape error:', error));
-};
+        return true;
+    }
+    else if (message.action === 'getData') {
+
+        chrome.runtime.sendNativeMessage('com.me.my_workday', { action: '/scrape' }, (response) => {
+            if (chrome.runtime.lastError) {
+                console.error('Native messaging error:', chrome.runtime.lastError.message);
+            }
+
+            console.log('Received from native app:', response);
+            sendResponse(response.result);
+        })
+
+        return true;
+    }
+    else if (message.action === 'addSite') {
+        chrome.runtime.sendNativeMessage('com.me.my_workday', { action: '/add-site', data: message.data }, (response) => {
+            console.log('addsite command received');
+            if (chrome.runtime.lastError) {
+                console.error('Native messaging error:', chrome.runtime.lastError.message);
+            }
+
+            console.log('Received add site data from native app:', response);
+            sendResponse(response.result);
+        })
+    }
+    else {console.log('unknown command'); sendResponse('command unknown')}
+});
 
 
